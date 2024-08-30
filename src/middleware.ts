6ffacb5f4670603f1fa35419d9103b9244ecd4bb
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-30 20:28:21 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  console.log(pathname);
+  const apiUrl = pathname.replace(/\api\//, "");
+  console.log(`Đang gọi đến ${apiUrl}, logic refresh token ở đây nếu cần`);
   return NextResponse.next();
 }
 export const config = {
-  matcher: "/(.*)/api/(.*)/", // */api/*
+  matcher: "/api/:path*", // */api/*
 };
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,6 @@
 export const fetchUser = async () => {
   const response = await fetch(`${process.env.API_URL}/api/me`, {
-    cache: "no-store", // Điều này ngăn không cho Next.js cache kết quả
+    cache: "no-store", // ngăn không cho next cache kết quả
   });
   const json = await response.json();
   const user = json.data;
