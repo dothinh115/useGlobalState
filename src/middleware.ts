6b724bc6e2e7f8bbe279560672b8b57864464771
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-30 21:19:06 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import type { NextRequest } from "next/server";
+import { REFRESH_TOKEN, TOKEN_EXPIRED_TIME } from "./utils/constant";
 
 export function middleware(req: NextRequest) {
-  const expTime = req.cookies.get("expTime"); //lấy exp time để so sánh
+  const expTime = req.cookies.get(TOKEN_EXPIRED_TIME); //lấy exp time để so sánh
+  const refreshToken = req.cookies.get(REFRESH_TOKEN); //lấy refresh token
   const currentTime = Math.floor(Date.now() / 1000);
   if (!expTime || (expTime && Number(expTime) < currentTime)) {
     //Token hết hạn, gọi refresh token
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,3 @@
 export const USER = "user-info";
+export const TOKEN_EXPIRED_TIME = "expiredTime";
+export const REFRESH_TOKEN = "refreshToken";
