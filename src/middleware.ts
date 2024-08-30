46c8417c30d3ebe837a40aacbbbe606561db890c
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-30 21:00:14 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const apiUrl = pathname.replace(/\api\//, "");
   const expTime = req.cookies.get("expTime"); //lấy exp time để so sánh
   const currentTime = Math.floor(Date.now() / 1000);
   if (Number(expTime) < currentTime) {
