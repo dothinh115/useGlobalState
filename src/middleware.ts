5a31f05d6f1e39f37488415573e0f33fe21f4668
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-30 21:00:01 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,11 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const apiUrl = pathname.replace(/\api\//, "");
-  console.log(`Đang gọi đến ${apiUrl}, logic refresh token ở đây nếu cần`);
-  return NextResponse.next();
+  const expTime = req.cookies.get("expTime"); //lấy exp time để so sánh
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (Number(expTime) < currentTime) {
+    //Token hết hạn, gọi refresh token
+  }
 }
 export const config = {
   matcher: "/api/:path*", // */api/*
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,9 @@
+import { headers } from "next/headers";
+
 export const fetchUser = async () => {
   const response = await fetch(`${process.env.API_URL}/api/me`, {
     cache: "no-store", // ngăn không cho next cache kết quả
+    headers: headers(),
   });
   const json = await response.json();
   const user = json.data;
