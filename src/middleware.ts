b58083af440ffbcb6728c0eadc0d585d52ba761a
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-09-28 16:02:53 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,10 @@ import { NextResponse, type NextRequest } from "next/server";
 import { ACCESS_TOKEN, TOKEN_EXPIRED_TIME } from "@/utils/constant";
 import { isTokenValid, refreshTokenFunc } from "./utils/common";
 
-const staticFileExtensions =
-  /\.(jpg|jpeg|png|gif|svg|ico|css|js|woff|woff2|ttf|eot|js|css)$/i;
-
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
-  const { pathname } = url;
   let accessToken = req.cookies.get(ACCESS_TOKEN)?.value;
   const res = NextResponse.next();
-  if (staticFileExtensions.test(pathname) || !pathname.startsWith("/api"))
-    return res;
   const expTime = req.cookies.get(TOKEN_EXPIRED_TIME); //lấy exp time để so sánh
   const tokenValid = isTokenValid(expTime?.value ?? null);
 
@@ -30,3 +24,7 @@ export async function middleware(req: NextRequest) {
     },
   });
 }
+
+export const config = {
+  matcher: "/api/:path*",
+};
