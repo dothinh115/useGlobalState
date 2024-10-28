b55c4a81d5962013eac8ffb20938267200c4a40f
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-10-28 15:50:55 +0700

    update

--- src/utils/serverFetch.ts
+++ src/utils/serverFetch.ts
@@ -6,7 +6,10 @@ export default async function serverFetch<T = any>(
 ): Promise<T> {
   const clientHeaders = await headers();
   const target = new URL(url, process.env.APP_URL).toString();
-
+  clientHeaders.delete("host");
+  clientHeaders.delete("connection");
+  clientHeaders.delete("content-length");
+  clientHeaders.delete("accept-encoding");
   try {
     const response = await fetch(target, {
       ...(clientHeaders && {
@@ -18,7 +21,7 @@ export default async function serverFetch<T = any>(
     const data: T = await response.json();
     return data;
   } catch (error) {
-    console.error("Server fetch thất bại: ", error);
+    console.error("Failed to fetch data:", error);
     throw error;
   }
 }
